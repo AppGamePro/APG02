8f98391c7e7ac51c8c016b03709e5a2ad4a80375
甘利聡 <swkaen@amarisatoshi-no-MacBook-Pro.local>
2015-05-17 17:22:17 +0900

    first commit

---
+++ Assets/create.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class create : MonoBehaviour {
+	public GameObject[] mino;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+				System.Random rnd= new System.Random(); 
+				int n = rnd.Next(7);
+
+				Instantiate(mino[n],transform.position,transform.rotation);
+		}
+
+	//void OnTriggerEnter2D () {}
+
+	
+	}
+}
